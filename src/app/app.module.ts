1d368d9a22a4b598a7559a25119fbab6b21944af
andres174 <andresfer.zambrano@espam.edu.ec>
2022-10-17 22:12:00 -0500

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { GradesComponent } from './pages/grades/grades.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    GradesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
